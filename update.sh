efd328f0350d7ad9fad97cb0cbcf95a91e83ffc3
Juanpujol674 <juanpujol674@gmail.com>
2024-10-14 21:16:45 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -115,7 +115,7 @@ install_feeds() {
         trojan-plus simple-obfs shadowsocksr-libev luci-app-passwall alist luci-app-alist smartdns luci-app-smartdns \
         v2dat mosdns luci-app-mosdns adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm \
         luci-lib-taskd luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
-        luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky
+        luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-mihomo
 }
 
 fix_default_set() {
