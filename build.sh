86e43e9afa56b2440dbbd3cbafe8b547abad98df
zqinking <zqinking>
2024-10-18 12:16:20 +0000

    remove mesh

--- build.sh
+++ build.sh
@@ -53,5 +53,5 @@ make -j$(nproc) || make -j1 || make -j1 V=s
 FIRMWARE_DIR="$BASE_PATH/firmware"
 \rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" \) -exec mv -f {} "$FIRMWARE_DIR/" \;
+find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
 \rm -f "$BASE_PATH/firmware/Packages.manifest" 2>/dev/null
