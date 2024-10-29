ee93d2a288234ac1dfb920c8172f83d6f7dc177e
zqinking <zqinking>
2024-10-29 09:56:08 +0800

    update wifi defaulte set

--- patches/992_set-wifi-uci.sh
+++ patches/992_set-wifi-uci.sh
@@ -20,15 +20,15 @@ configure_wifi() {
     uci set wireless.radio${radio}.country='US'
     uci set wireless.radio${radio}.txpower="${txpower}"
     uci set wireless.radio${radio}.disabled='0'
-    uci set wireless.default_radio${radio}.encryption='psk2+ccmp'
     uci set wireless.default_radio${radio}.ssid="${ssid}"
+    uci set wireless.default_radio${radio}.encryption='psk2+ccmp'
     uci set wireless.default_radio${radio}.key="${key}"
     uci set wireless.default_radio${radio}.ieee80211k='1'
     uci set wireless.default_radio${radio}.time_advertisement='2'
     uci set wireless.default_radio${radio}.time_zone='CST-8'
+    uci set wireless.default_radio${radio}.bss_transition='1'
     uci set wireless.default_radio${radio}.wnm_sleep_mode='1'
     uci set wireless.default_radio${radio}.wnm_sleep_mode_no_keys='1'
-    uci set wireless.default_radio${radio}.bss_transition='1'
 }
 
 jdc_ax1800_pro_wifi_cfg() {
