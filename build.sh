a3dbb0f4793f30a3b222f0ae3ba40ea0650d24aa
ZqinKing <zqinking23@gmail.com>
2024-12-10 11:54:06 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -55,5 +55,5 @@ make -j$(nproc) || make -j1 V=s
 FIRMWARE_DIR="$BASE_PATH/firmware"
 \rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.img.gz" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
 \rm -f "$BASE_PATH/firmware/Packages.manifest" 2>/dev/null
