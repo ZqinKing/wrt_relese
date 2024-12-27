d0a9b652b00b34f6d2087e3bb9134a17bf237db5
ZqinKing <zqinking23@gmail.com>
2024-12-27 02:41:35 +0000

    remove dns2tcp

--- update.sh
+++ update.sh
@@ -93,7 +93,7 @@ remove_unwanted_packages() {
         "luci-app-openclash" "luci-app-mihomo"
     )
     local packages_net=(
-        "haproxy" "xray-core" "xray-plugin" "dns2tcp" "dns2socks" "alist" "hysteria"
+        "haproxy" "xray-core" "xray-plugin" "dns2socks" "alist" "hysteria"
         "smartdns" "mosdns" "adguardhome" "ddns-go" "naiveproxy" "shadowsocks-rust"
         "sing-box" "v2ray-core" "v2ray-geodata" "v2ray-plugin" "tuic-client"
         "chinadns-ng" "ipt2socks" "tcping" "trojan-plus" "simple-obfs"
