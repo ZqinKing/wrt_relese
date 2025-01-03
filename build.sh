2818a61131d632cd3cf0612b50b2cdda2be1fdee
ZqinKing <zqinking23@gmail.com>
2025-01-03 02:46:11 +0000

    download uses more threads

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ if [[ -d $TARGET_DIR ]]; then
     find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*efi.img.gz" -o -name "*.itb" -o -name "*.fip" -o -name "*rootfs.tar.gz" \) -exec rm -f {} +
 fi
 
-make download -j$(nproc)
+make download -j$(($(nproc) * 2))
 make -j$(($(nproc) + 1)) || make -j1 V=s
 
 FIRMWARE_DIR="$BASE_PATH/firmware"
