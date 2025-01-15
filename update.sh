950b10e027aa80e68d6d7d285640c5770046f0f5
ZqinKing <zqinking23@gmail.com>
2025-01-15 00:57:51 +0000

    ipq60xx: The CPU scheduling policy is changed from performance to schedutil

--- update.sh
+++ update.sh
@@ -414,7 +414,7 @@ update_nss_pbuf_performance() {
     local pbuf_path="$BUILD_DIR/package/kernel/mac80211/files/pbuf.uci"
     if [ -d "$(dirname "$pbuf_path")" ] && [ -f $pbuf_path ]; then
         sed -i "s/auto_scale '1'/auto_scale 'off'/g" $pbuf_path
-        sed -i "s/scaling_governor 'schedutil'/scaling_governor 'performance'/g" $pbuf_path
+        sed -i "s/scaling_governor 'performance'/scaling_governor 'schedutil'/g" $pbuf_path
     fi
 }
 
