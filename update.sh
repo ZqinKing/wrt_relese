d66dac01db339f70163cf81edb1a0972963c8a3f
ZqinKing <zqinking23@gmail.com>
2025-03-09 13:34:13 +0000

    add open-app-filter

--- update.sh
+++ update.sh
@@ -140,7 +140,7 @@ install_small8() {
         adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd \
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
-        luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale
+        luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf
 }
 
 install_feeds() {
